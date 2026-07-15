20fd597f0f0f8b6fa64bdc3be6e422e15566f552
abe11075 <habibi@student.undiksha.ac.id>
2026-07-15 14:10:30 +0800

    Create ccd.lua

---
+++ ccd.lua
@@ -0,0 +1,104 @@
+{
+    "Support": {
+        "NoFishingAnimation": true,
+        "AutoEquipRod": true,
+        "BypassRadar": true,
+        "LockPosition": false,
+        "ShowPingPanel": true,
+        "DisableCutscenes": true,
+        "DisableFishNotif": true,
+        "DisableSkinEffect": true,
+        "WalkOnWater": true,
+        "AutoEquipDivingGear": true,
+        "HideOtherPlayers": false,
+        "DisableAbilityVFX": false
+    },
+    "StableResult": {
+        "Enabled": false
+    },
+    "SkipRarity": {
+        "Enabled": false,
+        "TargetRarities": ["None"]
+    },
+    "LegitFishing": {
+        "Enabled": false,
+        "ShakeDelay": 0.05,
+        "AutoShake": false
+    },
+    "InstantFishing": {
+        "Enabled": true,
+        "CastMode": "normal",
+        "CompleteDelay": 0.6
+    },
+    "InstantFastReel": {
+        "Enabled": false,
+        "CastMode": "normal",
+        "CompleteDelay": 0.02
+    },
+    "UltraBlatant": {
+        "Enabled": false,
+        "CompleteDelay": 0.3
+    },
+    "BlatantV2": {
+        "Enabled": false,
+        "SpamDelay": 0.05,
+        "CompleteDelay": 0.01,
+        "ChargeSpam": 2
+    },
+    "AutoCraft": {
+        "Enabled": false
+    },
+    "AutoSell": {
+        "Enabled": false,
+        "Mode": "ByCount",
+        "Value": 500
+    },
+    "AutoBuyWeather": {
+        "Enabled": true,
+        "Weathers": ["Fog", "Wind", "Storm"]
+    },
+    "AutoTreasureHunt": {
+        "Enabled": true,
+        "TweenSpeed": 50
+    },
+    "AutoClaimPirateChest": {
+        "Enabled": true
+    },
+    "AutoUsePotion": {
+        "Enabled": true,
+        "Potions": [
+            "Luck I Potion",
+            "Mutation III Potion",
+            "Luck II Potion"
+        ]
+    },
+    "AutoSpawnTotem": {
+        "Enabled": true,
+        "TotemName": "Mutation Totem"
+    },
+    "AutoSpawnTotem4x": {
+        "Enabled": false,
+        "Slot1": "",
+        "Slot2": "",
+        "Slot3": "",
+        "Slot4": ""
+    },
+    "AntiAFK": {
+        "Enabled": true
+    },
+    "AntiStaff": {
+        "Enabled": true
+    },
+    "Performance": {
+        "FPSBooster": true,
+        "AlwaysBright": true,
+        "Disable3DRendering": false,
+        "DisableWeatherVFX": true,
+        "DisableFishCaughtVFX": false,
+        "UnlockFPS": true,
+        "FPSCap": 240
+    },
+    "AutoReconnect": {
+        "Enabled": false
+    }
+}
